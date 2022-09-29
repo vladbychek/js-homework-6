b90d8f341377878693ac6137c4f1a792bad5f45e
Vlad <vladbychek3@gmail.com>
2022-09-29 16:27:19 +0300

    finished

--- task.js
+++ task.js
@@ -25,7 +25,7 @@ class Tamagochi {
          container.querySelector(".back").style.display = "block";
          container.style.border = "none";
       }
-    }, 20);
+    }, 1000);
   }
 
   updateHealth() {
@@ -39,20 +39,20 @@ class Tamagochi {
 function addTamagochi(name) {
   let newTamagochi = new Tamagochi(
     document.getElementById("choose-name").value,
-    (this.health = 100)
+    this.health = 100
   );
 
   tamagochiList.push(newTamagochi);
 
   name = document.getElementById("choose-name").value;
 
-  let tamagochu = document.createElement("div");
-  tamagochu.className = "tamagochi";
-  tamagochu.id = `tamagochi-${tamagochiList.length}`;
-  show.append(tamagochu);
+  let newDude = document.createElement("div");
+  newDude.className = "tamagochi";
+  newDude.id = `tamagochi-${tamagochiList.length}`;
+  show.append(newDude);
   newTamagochi.id = tamagochiList.length;
-  tamagochu.innerHTML =`
-   <div class="back">DEAD</div>
+  newDude.innerHTML =`
+    <div class="back">DEAD</div>
     <img class="tamagochi_img" src="tamagochi.jpg"> <div class="tamagochi_info"> 
     <div class="tamagochi_name">Name: ${name}<br> </div> 
     <div class="tamagochi_health">Health: <span class="health">${this.health}</span><br></div> 
@@ -63,7 +63,22 @@ function addTamagochi(name) {
 
   newTamagochi.healthEl = document.querySelectorAll(".health")[tamagochiList.length - 1];
 
-  feedButton.addEventListener("click", () => {
-    newTamagochi.feeding.bind(newTamagochi)();  
-  });
+  function feed() {
+    newTamagochi.feeding(newTamagochi)()
+  }
+  feedButton.addEventListener('click', feed)
+}
+
+document.querySelector('#create-btn').addEventListener('click', addTamagochi)
+
+
+const choose_inut = document.querySelector('#choose-name')
+const create_btn = document.querySelector('#create-btn')
+
+choose_inut.oninput = function() {
+  if(choose_inut.value != ''){
+    create_btn.removeAttribute('disabled')
+  } else {
+    create_btn.setAttribute('disabled', true)
+  }
 }
